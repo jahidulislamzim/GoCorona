356e1a2dd93e81421ef47d45618ad7ad8a8b26dd
jahidulislamzim <zaeemalfarabi845@gmail.com>
2022-02-12 19:36:18 +0600

    chnage schroll bar

--- js/script.js
+++ js/script.js
@@ -4,10 +4,11 @@ document.addEventListener( 'DOMContentLoaded', function() {
     classes: {
       pagination: 'splide__pagination currentPage',
       page      : 'splide__pagination__page pageClass',
+      arrow : 'splide__arrow cumtomArrowsClass',
     },
     type   : 'loop',
     perPage: 1,
-    autoplay:true,
+    // autoplay:true,
     interval:2000,
   });
   splide.mount();
